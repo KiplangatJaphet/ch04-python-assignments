532899c3ff348af0f41b73f9d5c25cf10943db9e
KiplangatJaphet <kiplangatjaphet2@gmail.com>
2025-08-30 23:34:29 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,43 @@
+# 1. Declare a variable named age and assign your age to it.
+age = 25
+print(age)
+print(type(age))
+
+#Output
+25
+<class 'int'>
+
+# 2. Create two variables x = 10 and y = 5. Print their sum.
+x = 10
+y = 5
+
+sum = x+y
+print(sum)
+
+#Output
+15
+
+# 3. Try using an invalid variable name like 2ndName and observe the error.
+2ndName = 'Japhet'
+print(2ndName)
+
+#Output
+Invalid decimal literal
+
+# 4. Assign a string to a variable and print it.
+script = 'Here we go!'
+print(script)
+
+#Output
+Here we go!
+
+#Challenge
+# Write a program that asks the user to input their name and age, and then prints a greeting.
+name = input ("Enter your name: ")
+age = input ("Enter your age: ")
+print (f"Hello", name, " you are "   ,  age,    " years old!")
+
+#Output
+Enter your name: Sam
+Enter your age: 23
+Hello Sam  you are  23  years old!
